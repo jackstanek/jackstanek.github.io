17e8c5de8b5bc6cde3f3facbd45c1bc9e1d416ca
Jack Stanek <stane064@umn.edu>
2018-01-24 11:28:01 -0600

    Update materials

--- viewport.js
+++ viewport.js
@@ -1,4 +1,5 @@
 var section_names;
+var sections;
 
 function populate_section_names() {
     section_names = new Array();
@@ -8,22 +9,41 @@ function populate_section_names() {
     }
 }
 
+function populate_sections() {
+    sections = new Array();
+    var sections_html_coll = document.getElementsByClassName("content");
+    for (var i = 0; i < sections_html_coll.length; i++) {
+        sections[i] = sections_html_coll[i];
+    }
+}
+
 /* called when section name is clicked */
 function select_section_name(selected_elem_id) {
     section_names.forEach(function (elem) {
+        /* Set active section name */
         if (elem.id == selected_elem_id &&
             !elem.classList.contains("active-section-name")) {
             elem.classList.add("active-section-name");
         } else if (elem.id != selected_elem_id) {
             elem.classList.remove("active-section-name");
         }
-        /* TODO: change active content */
+    });
+
+    sections.forEach(function (elem) {
+        /* Set active section name */
+        if (elem.id == selected_elem_id &&
+            !elem.classList.contains("active-content")) {
+            elem.classList.add("active-content");
+        } else if (elem.id != selected_elem_id) {
+            elem.classList.remove("active-content");
+        }
     });
 }
 
 /* add event listener to each section name */
 function init_nav() {
     populate_section_names();
+    populate_sections();
     section_names.forEach(function(elem) {
         elem.addEventListener("click", function () {
             select_section_name(elem.id);
